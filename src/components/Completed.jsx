d9ec844d9684d7153b8aaa8a449278e2dc3b5641
Yummy-Brown <rotimibrown8@gmail.com>
2024-06-06 13:32:16 +0100

    first commit

---
+++ src/components/Completed.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Progress from "./Progress";
+import { MdOutlineModeEditOutline } from "react-icons/md";
+import { RiDeleteBinLine } from "react-icons/ri";
+import { Link } from "react-router-dom";
+
+const Goal = ({ title, description, _id }) => {
+  return (
+    <div className="border-bottom border-3 border-secondary-subtle pb-4 px-4 mt-4 shadow-sm">
+      <p className="text-secondary fw-light"> {`Congratulations 🎉`} </p>
+
+      <h2 className="fw-bold text-capitalize">{title}</h2>
+      <p>{description} </p>
+      <div className="d-block d-md-flex align-items-end justify-content-between">
+        <Progress num={100} />
+        <div className="mt-2 mt-lg-0 d-flex gap-4">
+          <button className="blue-bg p-2 editbtn">
+            <Link
+              to={`/update/${_id}`}
+              className="text-decoration-none text-white"
+            >
+              {" "}
+              <MdOutlineModeEditOutline />
+              Edit{" "}
+            </Link>
+          </button>
+          <button className="p-2 border-none bg-transparent transparent delbtn">
+            <RiDeleteBinLine /> Delete{" "}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Goal;
---
+++ src/pages/Allgoals.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import GoalHeader from "../components/GoalHeader";
+
+import Goal from "../components/Goal";
+import Loading from "../components/Loading";
+import { useFetch } from "../Hooks/useFetch";
+import Empty from "../components/Empty";
+import ErrorFetch from "../components/ErrorFetch";
+import Goals from "../data/goals";
+
+const Allgoals = () => {
+  const url = 'https://goal-on-yummy-brown.onrender.com/api/v1/goals'
+  const {isLoading, isError, data:{goals : Goals}} = useFetch(url)
+  if (!isLoading && isError) {
+    return <ErrorFetch />
+  }
+  if (!isLoading && Goals.length === 0) {
+    return <Empty />
+  }
+  return (
+    <div className="container pb-3">
+      <GoalHeader heading="All Goals" />
+      {isLoading && <Loading />}
+      <div>
+        <div>
+          {Goals &&
+            Goals.map((g) => {
+              return <Goal key={g._id} {...g} />;
+            })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Allgoals;
